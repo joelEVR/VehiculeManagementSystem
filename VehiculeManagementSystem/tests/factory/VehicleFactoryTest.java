c9fcfa22319bd99336af81830b3515315c2371a6
joelEVR <joel.velasquez.ca@gmail.com>
2024-02-11 22:48:58 -0500

    test organized in package

--- VehiculeManagementSystem/tests/factory/VehicleFactoryTest.java
+++ VehiculeManagementSystem/tests/factory/VehicleFactoryTest.java
@@ -1,3 +1,4 @@
+package factory;
 import org.junit.Test;
 import static org.junit.Assert.*;
 import builder.CarBuilder;
--- VehiculeManagementSystem/tests/singleton/VehicleManagerTest.java
+++ VehiculeManagementSystem/tests/singleton/VehicleManagerTest.java
@@ -1,3 +1,4 @@
+package singleton;
 import static org.junit.Assert.*;
 import org.junit.Test;
 
